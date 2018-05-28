dbcff8192ebd3d2ac4bd12168bbab8363fc78b18
Scott Hall <scotthall127@gmail.com>
2018-05-28 05:31:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def intialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def intialize (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def intialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def intialize(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+end
